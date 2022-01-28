c0385b5c47782958a50b5383f8aef024f4ed8b26
준규 <vpark45@gmail.com>
2022-01-28 18:53:45 +0900

    Add type signature of function translateWord and holes

--- src/Exercise6.hs
+++ src/Exercise6.hs
@@ -1,5 +1,10 @@
+{-# OPTIONS_GHC -fdefer-typed-holes #-}
+
 substituteChar :: Char -> Char
 substituteChar c =
   case c of
     'e' -> '3'
     _   -> c
+
+translateWord :: String -> String
+translateWord word = _

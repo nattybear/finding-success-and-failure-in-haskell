9fd3fa1c6e2891ab3c03644a6adb570f1d15108f
준규 <vpark45@gmail.com>
2022-01-29 22:22:41 +0900

    Write body of function checkPasswordLength

--- src/validation-book/src/Main.hs
+++ src/validation-book/src/Main.hs
@@ -1,6 +1,10 @@
 module Main where
 
 checkPasswordLength :: String -> Maybe String
+checkPasswordLength password =
+  case (length password > 20) of
+    True  -> Nothing
+    False -> Just password
 
 main :: IO ()
 main = do

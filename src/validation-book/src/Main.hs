0dd11dd49f1cbfa0c8eb96bf070b6e951590bedc
준규 <vpark45@gmail.com>
2022-01-29 22:30:10 +0900

    Add function requireAlphaNum

--- src/validation-book/src/Main.hs
+++ src/validation-book/src/Main.hs
@@ -1,10 +1,18 @@
 module Main where
 
+import Data.Char
+
 checkPasswordLength :: String -> Maybe String
 checkPasswordLength password =
   case (length password > 20) of
     True  -> Nothing
     False -> Just password
+
+requireAlphaNum :: String -> Maybe String
+requireAlphaNum password =
+  case (all isAlphaNum password) of
+    False -> Nothing
+    True  -> Just password
 
 main :: IO ()
 main = do

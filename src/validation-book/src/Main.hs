e2b5a1cb27d83b5872d237b46f1dfe3389e1b28c
준규 <vpark45@gmail.com>
2022-02-01 20:16:18 +0900

    Simplify the code by removing that last case

--- src/validation-book/src/Main.hs
+++ src/validation-book/src/Main.hs
@@ -42,9 +42,7 @@ validatePassword password =
       case (requireAlphaNum password2) of
         Nothing -> Nothing
         Just password3 ->
-          case (checkPasswordLength password3) of
-            Nothing -> Nothing
-            Just password4 -> Just password4
+          checkPasswordLength password3
 
 main :: IO ()
 main = do

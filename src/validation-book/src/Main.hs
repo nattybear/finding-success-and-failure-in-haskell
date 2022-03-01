85af1e219fff6357b3ccdce5f0751a82235c2766
준규 <vpark45@gmail.com>
2022-03-01 11:29:41 +0900

    Add text to .cabal file and add import list

--- src/validation-book/src/Main.hs
+++ src/validation-book/src/Main.hs
@@ -4,6 +4,8 @@ module Main where
 
 import Data.Char
 import Data.Validation
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
 
 newtype Password = Password String
   deriving Show

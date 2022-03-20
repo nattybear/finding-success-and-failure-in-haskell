b43831690828a9d36f2593f2c97eed5be4adb5e9
준규 <vpark45@gmail.com>
2022-03-20 22:12:22 +0900

    Define MaybeAB class and instances

--- src/Chapter11.hs
+++ src/Chapter11.hs
@@ -11,3 +11,19 @@ instance LiftAB Validation where
 instance LiftAB Either where
   liftA = Left
   liftB = Right
+
+class MaybeAB f where
+  maybeA :: f a b -> Maybe a
+  maybeB :: f a b -> Maybe b
+
+instance MaybeAB Validation where
+  maybeA (Failure  a) = Just a
+  maybeA (Success _b) = Nothing
+  maybeB (Failure _a) = Nothing
+  maybeB (Success  b) = Just b
+
+instance MaybeAB Either where
+  maybeA (Left   a) = Just a
+  maybeA (Right _b) = Nothing
+  maybeB (Left  _a) = Nothing
+  maybeB (Right  b) = Just b
